fe95367e5ebe63bbc1e214152be15ee20defa659
DieOfMilk <milk@unist.ac.kr>
2020-10-22 23:23:18 +0900

    update RCRS env package

--- script/gym-RCRS/gym_RCRS/envs/RCRS_env.py
+++ script/gym-RCRS/gym_RCRS/envs/RCRS_env.py
@@ -27,76 +27,139 @@ class RCRSEnv(gym.Env):
         self.mapName=mapName
         # self.BuildingList = BuildingList
         self.verbose = verbose
-        self.buildingNo = buildingNo
+        self.buildingIdList = [871,877,883,885,886,887,888,889,894,895,896,904,908,909,913,915,916,920,923,925,927,
+928,1029,1039,1049,1060,1094,1104,1118,1128,1140,1151,1162,1173,1196,1201,1203,1208,1215,
+1221,1222,1233,1280,1281,1294,1306,1307,1314,1319,1329,1330,1335,1346,1350,1353,1361,1363]#removed 906
+        self.buildingNo = len(self.buildingIdList)
         self.maxTimeStamp = maxTimeStamp
         self.portNo = str(portNo)
         self.grpcNo = str(grpcNo)
-        self.maxWater = 25000
-        self.agentList = [1962675462,210552869]
-        self.action_space = spaces.MultiDiscrete([self.buildingNo+2]*len(self.agentList))
-        # self.observation_space = spaces.Box(low=np.array([0]*(buildingNo*3+len(self.agentList))+[-inf,-inf]*len(self.agentList)+[1]*len(self.agentList)).astype(int),high=np.array([inf]*(buildingNo*3)+[self.maxWater]*len(self.agentList)+[inf,inf]*len(self.agentList)+[2]*len(self.agentList)).astype(int),dtype= np.int) ## id, is on fire
-        self.observation_space = spaces.Box(low=np.array([0]*(buildingNo*1+len(self.agentList))+[0]*len(self.agentList)).astype(int),high=np.array([1]*(buildingNo*1)+[1]*len(self.agentList)+[1]*len(self.agentList)).astype(int),dtype= np.int) ## id, is on fire
+        self.maxWater = 30000
+        self.agentList = [1694383525,1762940799]
+        self.action_space = spaces.Discrete(self.buildingNo+2)
+        self.observation_space = spaces.Box(low=np.array([0]*(self.buildingNo*2+len(self.agentList))+[0]*(len(self.agentList)-1)).astype(int),high=np.array([1]*(self.buildingNo*2)+[1]*len(self.agentList)+[1]*(len(self.agentList)-1)).astype(int),dtype= np.int)
         self.obs= {}
-        self.buildingIdList = [247, 248, 249, 250, 251, 253, 254, 255, 905, 934, 935, 936, 937, 938, 939, 940, 941, 942, 943, 
-        944, 945, 946, 947, 948, 949, 950, 951, 952, 953, 954, 955, 956, 957, 958, 959, 960] # removed 298
+         # removed 298
         seedNo=23
         self.np_random , seed = gym.utils.seeding.np_random(seedNo)
         self.kernel = None
         self.agent = None
-        self.reward = 0
+        self.reward = [0,0]
+        self.action = [None,None]
+        self.prevObs = None
         
 
-    def step(self,action):
+    def step(self):
         # self.timeStamp +=1
         self.total_step +=1
         print("start step. Total step is : {}".format(self.total_step))
-        self.run_action(action)
         self.done= False
-        self.connection.set_step_finished()
-        self.wait_request(900,0.1)
-        self.obs = self.connection.getObs(900,0.1)
-        self.obs=np.append(self.obs,self.connection.getBusy(900,0.1))
-        print(self.obs)
-        self.reward = self.getReward()
-        print(self.reward)
-        if self.timeStamp==self.maxTimeStamp:
-            print("finished max time stamp")
-            self.run_action([self.buildingNo,self.buildingNo])
+        self.run_action()
+        while True:
+            self.prevObs=self.obs.copy()
             self.connection.set_step_finished()
+            self.wait_request(900,0.1)
+            self.obs = self.connection.getObs(900,0.1)
+            self.busy = self.connection.getBusy(900,0.1)
+            self.reward = self.getReward()
+            if np.isin(2,self.busy) or self.timeStamp == self.maxTimeStamp:
+                break
+        # print(self.prevObs)
+        if self.busy[0] == 2 and self.busy[1] == 2:
+            self.idNumber =3
+            self.action[0] = None
+            self.action[1] = None
+            self.obs=np.append(self.obs,self.busy[1])
+        elif self.busy[0] == 2:
+            self.idNumber = 1
+            self.action[0] = None
+            self.obs=np.append(self.obs,self.busy[1])
+        elif self.busy[1] == 2:
+            self.idNumber = 2
+            self.action[1] = None
+            self.obs=np.append(self.obs,self.busy[0])
+        else: ## timestamp finished
+            self.idNumber = 4  
+            self.obs=np.append(self.obs,self.busy[1])
+        # if self.idNumber == 2:
+        #     self.obs = self.convertObs()
+        print(self.action)
+        print(self.idNumber)
+        print(self.obs)
+        print("the reward is ",self.reward)
+        if self.timeStamp>=self.maxTimeStamp:
+            print("finished max time stamp")
+            while self.idNumber != 3:
+                self.input(self.buildingNo)
+                self.run_action()
+                self.connection.set_step_finished()
+                self.wait_request(900,0.1)
+                trashobs = self.connection.getObs(900,0.1)
+                trashbusy = self.connection.getBusy(900,0.1)
+                trashobs=np.append(trashobs,trashbusy[1])
+                if (trashbusy[0] == 2 and trashbusy[1] == 2) or self.timeStamp==50: ## wait until agent finished but ignore the changed env after 35 time step.
+                    self.idNumber = 3
+                    break
             self.done=True
         info = {}
-        if not np.isin(1,self.obs[0:-4]): # about remaining fire
+        if not np.isin(1,self.obs[0:-(2*len(self.agentList)-1):2]): # about remaining fire
             info['is_success']=True   
         else:
             info['is_success']=False
+        firenumber = 0
+        isonfire = np.array(self.obs[0:-(2*len(self.agentList)-1):2])
+        for i in isonfire:
+            if i==1:
+                firenumber +=1
+        info['fireNumber'] =  firenumber
+        if self.done:
+            info['finalFireNumber'] =  firenumber
+        info['idNumber'] = self.idNumber
         final_obs = self.normalization(self.obs)
         print(final_obs)
-        return final_obs, self.reward, self.done, info
-    def run_action(self, action):
+        returnReward = self.reward.copy()
+        returnReward[0] = returnReward[0]/50
+        returnReward[1] = returnReward[1]/50
+        if self.idNumber == 3:
+            self.reward = [0,0]
+        elif self.idNumber == 1:
+            self.reward[0] = 0
+        elif self.idNumber == 2:
+            self.reward[1] = 0
+        return [final_obs], [returnReward], [self.done], [info]
+    def run_action(self):
         action_list = []
-        for i in action:
+        for i in self.action:
             if i == self.buildingNo:
                 action_list.append("R")
             elif i == self.buildingNo+1:
                 action_list.append("C")
             # elif i == self.buildingNo+2:
             #     action_list.append("C")
+            elif i!=None:
+                action_list.append("S "+ str(self.buildingIdList[i])) 
             else:
-                action_list.append("S "+ str(self.buildingIdList[i]))    
+                print("run action error")
+                exit(1) 
         self.connection.inputAction(action_list)
 
     def reset(self):
+        self.prevObs = None
+        self.action = [None,None]
+        self.busy = [2,2]
+        self.reward = [0,0]
+        self.idNumber=3
         gc.collect()
         print("start reset")
         self.server = None
         self.connection = None
         self.timeStamp = -1
-        time.sleep(3)
+        time.sleep(1)
         self.server, self.connection  = self.serve(self.grpcNo)
         
         logName = self.mapName
         
-        origin_map_path = './../rcrs-server/maps/gml/EnvTest'
+        origin_map_path = './../rcrs-server/maps/gml/bigTest2'
         map_path = './../rcrs-server/maps/gml/' + logName
         try:
             shutil.copytree(origin_map_path, map_path)
@@ -129,7 +192,7 @@ class RCRSEnv(gym.Env):
         else:
             self.kernel = subprocess.Popen("./start.sh -l {} -m {} -c {} -p {} -r {} -g".format(logpath,mapfile,common_path,self.portNo,self.grpcNo).split(), 
             shell=False, cwd = "./../rcrs-server/boot")
-        time.sleep(16)
+        time.sleep(8)
         self.connection.set_step_finished() 
         self.agent = subprocess.Popen("./launch.sh -all -s localhost:{} -r {}".format(self.portNo, self.grpcNo).split(),shell=False, cwd = "./../rcrs-grpc-demo")
         for _ in range(1):
@@ -141,10 +204,36 @@ class RCRSEnv(gym.Env):
                 self.agent.wait()
                 self.agent = subprocess.Popen("./launch.sh -all -s localhost:{} -r {}".format(self.portNo, self.grpcNo).split(),shell=False, cwd = "./../rcrs-grpc-demo")
         self.obs = self.connection.getObs(900,0.1)
-        self.obs=np.append(self.obs,self.connection.getBusy(900,0.1))
-        self.step([self.buildingNo,self.buildingNo])
-        self.step([self.buildingNo,self.buildingNo])
+        self.busy = self.connection.getBusy(900,0.1)
+        self.obs=np.append(self.obs,self.busy)
+        self.input(self.buildingNo)
+        self.input(self.buildingNo)
+        self.step()
+        self.input(self.buildingNo)
+        self.input(self.buildingNo)
+        self.step()
+        print("reset finished")
         return self.obs
+    def input(self,action):
+        if self.action[0] == None:
+            self.action[0] = action
+            return True
+        elif self.action[1] == None:
+            self.action[1] = action
+            return True
+        else:
+            print("No empty input")
+            return False
+    
+    def wait_busy(self, timeout, period):
+        must_end = time.time() + timeout
+        while time.time() < must_end:
+            if np.isin(2,self.connection.copyBusy(timeout, period)):
+                print("At least one agent is idle")
+                return self.connection.copyBusy(timeout, period)
+            time.sleep(period)
+        print("aget wait busy error")
+        exit(1)
 
     def render(self):
         self.timeStamp = -1
@@ -205,14 +294,30 @@ class RCRSEnv(gym.Env):
         # exit()
         return False
     def getReward(self):
-        firenumber = 0
-        isonfire = np.array(self.obs[0:-4])
-        for i in isonfire:
-            firenumber +=i
-        self.reward = (36-firenumber)/36
+        isonfire = np.array(self.obs[0:-len(self.agentList)])
+        print(self.obs)
+        wasonfire = np.array(self.prevObs[0:-len(self.agentList)])  ## get the building information part
+        print(self.prevObs)
+        for i in range(self.buildingNo):
+            if (wasonfire[i*2+1] < 5 and isonfire[i*2+1] >= 5) or wasonfire[i*2+1] > isonfire[i*2+1]:
+                self.reward[0] +=1
+                self.reward[1] +=1
+            elif (isonfire[i*2+1] < 5) and (wasonfire[i*2+1] < isonfire[i*2+1] or wasonfire[i*2+1] >= 5):
+                print("the negative one is ", i)
+                self.reward[0] -=1
+                self.reward[1] -=1
+        if (self.prevObs[-2]<5000 and self.obs[-2]>5000) or (self.prevObs[-1]<5000 and self.obs[-1]>5000):
+              self.reward[0]+=1
+              self.reward[1]+=1
+        print(self.reward)
         return self.reward
     def getObs(self):
         return self.obs
+    def convertObs(self):
+        tempobs = self.obs[:-1]
+        tempobs[-1], tempobs[-2] = tempobs[-2],tempobs[-1]
+        tempobs = np.append(tempobs,self.busy[0])
+        return self.normalization(tempobs)
     def killprocess(self):
         print("start kill process")
         if self.kernel:
@@ -220,14 +325,19 @@ class RCRSEnv(gym.Env):
             self.kernel.wait()
         if self.agent:
             self.agent.terminate()
-            self.kernel.wait()
+            self.agent.wait()
+        del self.connection
+        self.connection = None
     def normalization(self, obs):
         final_obs = []
         for i in range(len(obs)):
-            if i< len(obs) - 2*len(self.agentList):
-                final_obs.append(obs[i])
+            if i< len(obs) - (2*len(self.agentList)-1):
+                if i%2 == 0:
+                    final_obs.append(obs[i])
+                else:
+                    final_obs.append(obs[i]/8)
             else:
-                if i < len(obs) - len(self.agentList):
+                if i < len(obs) - (len(self.agentList)-1):
                     final_obs.append(obs[i]/self.maxWater)
                 else:
                     final_obs.append(obs[i]-1)
@@ -258,6 +368,8 @@ class SimpleConnection(RCRS_pb2_grpc.SimpleConnectionServicer):
                 # print("yes")
                 self.busycheck[i] = 1
                 self.busy[i] = request.Busy
+                if self.busy[i] == 2:
+                    self.action[i] = None
                 # print("check busy from agent")
                 break
             # else:
@@ -274,50 +386,54 @@ class SimpleConnection(RCRS_pb2_grpc.SimpleConnectionServicer):
             time.sleep(0.5)
             return RCRS_pb2.ActionType(actionType= 4, x=float(0), y=float(0))
         # time.sleep(10)
-        check = self.wait_action(900,0.1)
-        if not check:
-            print("no action input")
-            exit()
         for i in range(len(self.agentList)):
             if request.AgentID == self.agentList[i]:
-                templist = self.action[i].split(" ")
-                self.action[i] = None
-                break
-        for i in range(len(self.agentList)):
-            if self.agentList[i] == request.AgentID:
-                if templist[0] == "S":
-                    x = templist[1]
-                    y = "0"
-                    return RCRS_pb2.ActionType(actionType= 1, x=float(x), y=float(y))
-                elif templist[0] == "M":
-                    x="0"
-                    y="0"
-                    return RCRS_pb2.ActionType(actionType= 2, x=float(x), y=float(y))
-                elif templist[0] == "C":
-                    x="0"
-                    y="0"
-                    return RCRS_pb2.ActionType(actionType= 3, x=float(x), y=float(y))
-                else:
+                idNumber = i
+                if self.busy[i] == 1:
                     x="0"
                     y="0"
                     return RCRS_pb2.ActionType(actionType= 4, x=float(x), y=float(y))
+        check = self.wait_action(900,0.1, idNumber)
+        if not check:
+            print("no action input")
+            exit()
+        templist = self.action[idNumber].split(" ")
+        self.action[idNumber] = None
+        if templist[0] == "S":
+            x = templist[1]
+            y = "0"
+            return RCRS_pb2.ActionType(actionType= 1, x=float(x), y=float(y))
+        elif templist[0] == "M":
+            x="0"
+            y="0"
+            return RCRS_pb2.ActionType(actionType= 2, x=float(x), y=float(y))
+        elif templist[0] == "C":
+            x="0"
+            y="0"
+            return RCRS_pb2.ActionType(actionType= 3, x=float(x), y=float(y))
+        else:
+            x="0"
+            y="0"
+            return RCRS_pb2.ActionType(actionType= 4, x=float(x), y=float(y))
     def RunTimestep(self, request, context):
         ## should wait until previous timestep is finished
-        # check = self.wait_step_finish(150,0.1)
-        # if not check:
-            # print("previous step error")
+        check = self.wait_step_finish(150,0.1)
+        if not check:
+            print("previous step error")
+            exit(1)
         self.request = request
         self.timestamp = request.time
-        obs=np.zeros(1*len(self.buildingIdList),dtype=np.int)
+        obs=np.zeros(2*len(self.buildingIdList),dtype=np.int)
         temp = np.zeros(1*len(self.agentList),dtype=np.int)
         for i in request.areas:
             if i.uRN=="Building" or i.uRN=="AmbulanceCentre" or i.uRN=="FireStation" or i.uRN=="PoliceOffice" or i.uRN=="GasStation" or i.uRN=="Refuge":
                 for j in range(len(self.buildingIdList)):
                     if i.iD == self.buildingIdList[j]:
                         if i.isOnFire:
-                            obs[1*j]=1
+                            obs[2*j]=1
                         else:
-                            obs[1*j]=0
+                            obs[2*j]=0
+                        obs[2*j+1]=i.fieryness
                         break
         for i in request.humans:
             if i.uRN=="FireBrigade":
@@ -337,13 +453,15 @@ class SimpleConnection(RCRS_pb2_grpc.SimpleConnectionServicer):
     def getTimeStamp(self):
         return self.timestamp
     def inputAction(self, action):
-        self.action = action
+        for i in range(len(self.action)):
+            if self.action[i] == None:
+                self.action[i] = action[i]
         return True
-    def wait_action(self,timeout, period):
+    def wait_action(self,timeout, period, idNumber):
         must_end = time.time() + timeout
         while time.time() < must_end:
-            if self.action != [None]*len(self.agentList):
-                print("action get successfully")
+            if self.action[idNumber] != None:
+                print("action get successfully at {}".format(idNumber))
                 return True
             time.sleep(period)
         print("action get error")
@@ -367,10 +485,22 @@ class SimpleConnection(RCRS_pb2_grpc.SimpleConnectionServicer):
             if not np.isin(0,self.busycheck):
                 print("get Busy")
                 busy = self.busy.copy()
+                print(busy)
                 self.busycheck = np.zeros(len(self.agentList),dtype=np.int)
                 return busy
             time.sleep(period)
         print("busy get error")
+        exit()
+        return False
+    def copyBusy(self,timeout, period):
+        must_end = time.time() + timeout
+        while time.time() < must_end:
+            if not np.isin(0,self.busycheck):
+                print("get Busy")
+                busy = self.busy.copy()
+                return busy
+            time.sleep(period)
+        print("busy copy error")
         exit()
         return False
     def wait_step_finish(self, timeout, period):
