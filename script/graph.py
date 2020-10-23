6501e353cc93bab12669761a462eaadbfb4b6145
DieOfMilk <milk@unist.ac.kr>
2020-10-23 14:55:15 +0900

    Update graph.py

--- script/graph.py
+++ script/graph.py
@@ -16,55 +16,28 @@ import signal
 from numpy import inf
 import pickle
 from matplotlib import pyplot as plt
+import matplotlib
 
 if __name__=='__main__':
-    total=[]
-    for i in range(19):
-        path = os.path.join('.','RCRS{}/data.pkl'.format(i))
-        try:
-            with open (path,'rb') as f:
-                temp = pickle.load(f)
-            # data = np.asarray([])
-            # for i in temp:
-            #     data = np.append(data,temp)
-            # print(data.shape)
-            # # print(data[0])
-            # data = data.flatten()
-            total.append(temp)
-        except:
-            print("error happend")
-            pass
-    total_path = os.path.join('.','total')
-    with open(total_path, 'wb') as f:
-        pickle.dump(total,f)
-    # print(total)
-    # print(total)
-    # print(total[0])
-    # new_total=[]
-    # for i in total:
-    #     temp=[]
-    #     y = 0
-    #     for j in i:
-    #         if j>=y:
-    #             y=j
-    #         else:
-    #             temp.append(y)
-    #             y=0
-    #         if j==1:
-    #             print("there is 1")
-    #     temp2 = []
-    #     new_total.append(temp2)
-    # new_total_path = os.path.join('.','new_total')
-    # print(len(new_total))
-    # print(new_total[0])
-    # with open(new_total_path, 'wb') as f:
-    #     pickle.dump(new_total,f)
-    for i in [6]:
-        plt.plot(range(len(total[i])),total[i], label="Learning_rate {}\n Decay_rate {}".format(0.004,0.99))
-    plt.ylim([0,10])
-    plt.xlabel("Episode No.",fontsize=48)
-    plt.ylabel("Remained Fire No.",fontsize=48)
-    plt.legend(loc="upper right",fontsize=24)
+    result_path = os.path.join('.','result.pkl')
+    with open(result_path,'rb') as f:
+        total = pickle.load(f)
+    new_total=[]
+    for i in total:
+        new_total.append(sum(i)/3)
+    gready = []
+    for _ in range(10,990,20):
+        gready.append(-166)
+
+    matplotlib.rcParams.update({'font.size':30})
+
+    plt.plot(range(10,990,20),new_total, label="PPO")
+    plt.plot(range(10,990,20),gready, label="greedy methods")
+    plt.xlabel("Episode Number",fontsize=48)
+    plt.ylabel("Total Sum of Rewards",fontsize=48)
+    plt.xticks(range(0,1000,50))
+    plt.legend(loc="lower right",fontsize=24)
+    plt.gcf().subplots_adjust(top=0.96, bottom=0.122, right=0.98, left=0.11)
     plt.show()
 
 
